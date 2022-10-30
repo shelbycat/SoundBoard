2a252f1f02655e322e8d1241cba4024f455ee07e
ShelbyKauth <shelbykauth@gmail.com>
2022-10-30 19:20:27 -0400

    Install Material, Set Routes, Add favicon

--- soundboard/src/app/app-routing.module.ts
+++ soundboard/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './routes/about/about.component';
+import { MixerComponent } from './routes/mixer/mixer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'home', component: AboutComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'mixer', component: MixerComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- soundboard/src/app/app.module.ts
+++ soundboard/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MaterialModule } from './modules/material-module/material-module.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './routes/about/about.component';
+import { MixerComponent } from './routes/mixer/mixer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AboutComponent, MixerComponent],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
