0e6e6a4b275d0d031b5422d1a8886be27b702950
ShelbyKauth <shelbykauth@gmail.com>
2022-11-01 22:58:09 -0400

    Add Test Page

--- web-ui/src/app/app-routing.module.ts
+++ web-ui/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './routes/about/about.component';
 import { MixerComponent } from './routes/mixer/mixer.component';
+import { TestPageComponent } from './routes/test-page/test-page.component';
 
 const routes: Routes = [
   { path: '', component: AboutComponent },
   { path: 'home', component: AboutComponent },
   { path: 'about', component: AboutComponent },
   { path: 'mixer', component: MixerComponent },
+  { path: 'testPage', component: TestPageComponent },
   { path: '**', redirectTo: '' },
 ];
 
--- web-ui/src/app/app.module.ts
+++ web-ui/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './routes/about/about.component';
 import { MixerComponent } from './routes/mixer/mixer.component';
+import { TestPageComponent } from './routes/test-page/test-page.component';
 
 @NgModule({
-  declarations: [AppComponent, AboutComponent, MixerComponent],
+  declarations: [AppComponent, AboutComponent, MixerComponent, TestPageComponent],
   imports: [BrowserModule, AppRoutingModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
